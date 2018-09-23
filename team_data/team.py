2acf559b1405eb1d3ba1198dd4523a10c612826c
Tina Wu <tinawu.nd23@gmail.com>
2018-09-23 19:21:58 -0400

    feature plot team data

--- team_data/team.py
+++ team_data/team.py
@@ -15,7 +15,17 @@ i = 0
 
 for team in df['Team']:
 	df.at[i,'Playoff'] = "Y" if '*' in team else "N"
+	df.at[i,'Team'] = str(df.at[i,'Team']).replace('*','') 
 	i += 1
 
-print(df)
+# plot team based on two features; playoff in red; not playoff in blue
+n=0
+for team in df['Playoff']:
+	if team == 'Y':
+		plt.scatter(df.at[n,"FG%"],df.at[n,"AST"], marker='^', c='r')
+	elif team == 'N':
+		plt.scatter(df.at[n,"FG%"],df.at[n,"AST"], marker='+', c='b')
+	n += 1
+
+plt.show()
 
