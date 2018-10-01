f460b3d10595150cdecc700f676445130d530f25
oliviaroy20 <oliviaroy20@gmail.com>
2018-09-30 20:37:36 -0400

    Cleaned the team data files

--- team_data/team.py
+++ team_data/team.py
@@ -3,11 +3,11 @@
 import numpy as np
 import pandas as pd
 from scipy.stats import zscore
-from scipy.sparse.linalg import svds 
+from scipy.sparse.linalg import svds
 import matplotlib.pyplot as plt
 
 # read team data for a given year
-df = pd.read_csv('team_data_08_09.csv', nrows=30)
+df = pd.read_csv('team_data_17_18.csv', nrows=30)
 
 # add a column of whether team made playoff
 df['Playoff'] = ' '
@@ -15,17 +15,17 @@ i = 0
 
 for team in df['Team']:
 	df.at[i,'Playoff'] = "Y" if '*' in team else "N"
-	df.at[i,'Team'] = str(df.at[i,'Team']).replace('*','') 
+	df.at[i,'Team'] = str(df.at[i,'Team']).replace('*','')
 	i += 1
 
+df.to_csv("team_data_17_18.csv", index= False, float_format='%g')
 # plot team based on two features; playoff in red; not playoff in blue
-n=0
-for team in df['Playoff']:
-	if team == 'Y':
-		plt.scatter(df.at[n,"FG%"],df.at[n,"AST"], marker='^', c='r')
-	elif team == 'N':
-		plt.scatter(df.at[n,"FG%"],df.at[n,"AST"], marker='+', c='b')
-	n += 1
-
-plt.show()
-
+# n=0
+# for team in df['Playoff']:
+# 	if team == 'Y':
+# 		plt.scatter(df.at[n,"FG%"],df.at[n,"AST"], marker='^', c='r')
+# 	elif team == 'N':
+# 		plt.scatter(df.at[n,"FG%"],df.at[n,"AST"], marker='+', c='b')
+# 	n += 1
+#
+# plt.show()
