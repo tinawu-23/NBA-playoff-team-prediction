f2cae9736e90c76b4e10e79b0e0c9a2e82a78d8b
Tina Wu <tinawu.nd23@gmail.com>
2018-10-03 15:05:17 -0400

    new files for analysis

--- Models/f1_analysis.py
+++ Models/f1_analysis.py
@@ -23,6 +23,8 @@ def analysis(filename):
 	for i in range(len(lst)):
 		df.iloc[9,i+2] = lst[i]
 	print(df)
+	new_file = "new_"+filename
+	df.to_csv(new_file, index= False, float_format='%g')
 	sorted_diffdict = sorted(diffdict.items(), key=operator.itemgetter(1))
 	print(sorted_diffdict)
 
