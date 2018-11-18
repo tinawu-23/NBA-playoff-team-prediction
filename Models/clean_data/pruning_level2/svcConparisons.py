95228135ef15309ff07f988bda62c540b1840b2e
Francis Schickel <fschicke@nd.edu>
2018-11-18 15:44:59 -0500

    fixed bugs

--- Models/clean_data/pruning_level2/svcConparisons.py
+++ Models/clean_data/pruning_level2/svcConparisons.py
@@ -5,7 +5,7 @@ from sklearn import svm
 from evaluations import Evaluation
 
 def extractData(fileName, exclude):
-	df = pd.read_csv("../../../team_data/" + fileName)
+	df = pd.read_csv("../../../team_data/Removed_col/" + fileName)
 	columnsSelected = list(df.columns.values)
 	columnsSelected = columnsSelected[2:18]
 	possible_cols = [None]
@@ -15,9 +15,9 @@ def extractData(fileName, exclude):
 		if x is not None and x not in exclude:
 			columnsSelected.pop(x-popped)
 			popped += 1
+	print(columnsSelected)
 	data = df.loc[:, list(columnsSelected)]
 	labels = df["Playoff"]
-	print(data)
 	return data, labels
 
 if __name__ == '__main__':
@@ -25,11 +25,23 @@ if __name__ == '__main__':
 	teamData = pd.read_csv('../../../team_data/teamFiles.csv')
 	dummy_exclude = [3, 1, 2, 8, 15, 4, 7, 10, 13, 5, 11, 9, 12, 14, 6, 0]
 	exclusions = list()
+	no_inclusions = [None]
+	no_inclusions = list(range(0, 16))
 	d = {} #will be converted to dataframe
 	years = []
 	for i in range(0,9):
 		years.append(str(i+9) + "to" + str(i+10)) #add year column
 	d["Testing Data Season"] = years
+	f1 = []
+	for index, year in teamData.iterrows():
+		train_data, train_label = extractData(year["Year1"], no_inclusions)
+		test_data, test_label = extractData(year["Year2"], no_inclusions)
+		clf = svm.SVC()
+		y_pred = clf.fit(train_data,train_label)
+		predictions = clf.predict(test_data)
+		eval= Evaluation(predictions, test_label)
+		f1.append(round(eval.getF1(),5))
+		d["None"] = f1
 
 	for exclude in dummy_exclude:
 		exclusions.append(exclude)
@@ -42,10 +54,7 @@ if __name__ == '__main__':
 			predictions = clf.predict(test_data)
 			eval= Evaluation(predictions, test_label)
 			f1.append(round(eval.getF1(),5))
-			if exclude is None:
-				d["None"] = f1
-			else:
-				d[exclude] = f1 #f1 scores for each feature for a given year
+			d[exclude] = f1 #f1 scores for each feature for a given year
 	table = pd.DataFrame(d)
 
 	table.to_csv('f1_svc.csv', sep=',', index=False)
